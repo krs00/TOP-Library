7cdeb37d7311548a2e5cdd8d801a98d3507dbc97
Kymani Stephens <kymanist00@gmail.com>
2023-08-25 15:40:15 -0700

    add toggle read function

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ const myLibrary = [] // Empty array, holds all of the book objects
 function handleFormSubmit(e) { 
   e.preventDefault(); // stops default behavior of form
 
+  // catch form input values
   const title = document.getElementById('title').value
   const author = document.getElementById('author').value
   const pages = document.getElementById('pages').value
   const isRead = document.getElementById('read').checked
 
+  // create new book instance with form input values
   const newBook = new Book(title, author, pages, isRead)
 
+  // add new instance of book to myLibrary array 
   addBookToLibrary(newBook)
   console.log(myLibrary) 
 
@@ -24,7 +27,13 @@ function Book(title, author, pages, isRead) {
   this.title = title; 
   this.author = author;
   this.pages = pages;
-  this.isRead = isRead; 
+  this.isRead = isRead;
+
+  // function will toggle read status of individual book
+  this.toggleRead = function() {
+    this.isRead = !this.isRead 
+  } 
+
 }
 
 function addBookToLibrary(book) {
