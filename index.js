7ddf64dcd59fbac9ef3d2fa57daa5b60eb62ebfc
Kymani Stephens <kymanist00@gmail.com>
2023-08-25 16:13:17 -0700

    add addBookToDisplay

--- index.js
+++ index.js
@@ -15,21 +15,23 @@ function handleFormSubmit(e) {
   const isRead = document.getElementById('read').checked
 
   // create new book instance with form input values
-  const newBook = new Book(title, author, pages, isRead)
+  const newBook = new Book(title, author, pages, isRead) 
 
   // add new instance of book to myLibrary array 
   addBookToLibrary(newBook)
+  addBookToDisplay() 
+  console.log(newBook.title)
   console.log(myLibrary) 
 
 }
 
 function Book(title, author, pages, isRead) {
-  this.title = title; 
+  this.title = title;
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
 
-  // function will toggle read status of individual book
+  // function will toggle read status on individual instance of book
   this.toggleRead = function() {
     this.isRead = !this.isRead 
   } 
@@ -38,4 +40,21 @@ function Book(title, author, pages, isRead) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book) 
+}
+
+// finds the necessary index of last book added to library array
+// this is number is used to associate to the respective DOM element when it gets created
+function findLastIndex() {
+  return myLibrary.length - 1;
+}
+
+
+function addBookToDisplay() {
+  const dataNum = findLastIndex()
+  const div = document.createElement('div')
+  div.setAttribute('data-index', dataNum )
+  div.classList.add('book-div')
+
+  document.body.appendChild(div) 
+  
 }
